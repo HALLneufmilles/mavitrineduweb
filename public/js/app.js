5ea6192a1577202e534ff95dd4c3f7d2e794f57d
HALLneufmilles <hall9milles@gmail.com>
2023-11-27 15:25:50 +0100

    response to res

--- public/js/app.js
+++ public/js/app.js
@@ -248,7 +248,7 @@ sendEmailBtn.addEventListener("click", (event) => {
     }),
   })
     .then((res) => {
-      if (!response.ok) {
+      if (!res.ok) {
         throw new Error("Network response was not ok");
       }
       return res.json();
